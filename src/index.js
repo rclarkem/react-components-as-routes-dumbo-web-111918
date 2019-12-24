8556b4bc5919ccfa0d445c7301b4df6408da2b0d
Reina <r.clarkemitchell@gmail.com>
2019-12-23 19:47:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import About from "./About";
+import NavBar from "./NavBar";
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <divt>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </divt>
+  </Router>,
+  document.getElementById("root")
 );
